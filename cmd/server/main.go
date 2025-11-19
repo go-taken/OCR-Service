3583cf10d0f3a737b07308928cdbab79e23842ea
triadmoko <>
2025-11-19 18:38:39 +0700

    Remove unused memory allocation and print statement from main function in server implementation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"app/internal/server"
 )
 
 func main() {
-
-	mem := 50 << 20
-	fmt.Println(mem)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
